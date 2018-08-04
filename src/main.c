aae09828d2175e2f95db188c2d251090524fd1fb
uael <abel@tempow.com>
2018-08-04 16:28:33 +0200

    main loop norm

--- src/main.c
+++ src/main.c
@@ -12,94 +12,104 @@
 
 #include <wolf.h>
 #include <libft.h>
-#include <pthread.h>
-#include <semaphore.h>
 
 typedef struct	s_game
 {
 	t_bool		running;
-	sem_t		calculate;
-	pthread_t	calculator;
-	sem_t		render;
-	pthread_t	renderer;
+	SDL_sem		*calculate;
+	SDL_Thread	*calculator;
+	SDL_sem		*render;
+	SDL_Thread	*renderer;
 	SDL_Window	*win;
 }				t_game;
 
-static int wl_quit(void)
+static int wl_quit(t_game *game)
 {
 	const char *error;
 
+	game->running = 0;
+	game->calculate ? SDL_SemPost(game->calculate) : 0;
+	game->render ? SDL_SemPost(game->render) : 0;
+	game->calculator ? SDL_WaitThread(game->calculator, NULL) : 0;
+	game->renderer ? SDL_WaitThread(game->renderer, NULL) : 0;
+	game->calculate ? SDL_DestroySemaphore(game->calculate) : 0;
+	game->render ? SDL_DestroySemaphore(game->render) : 0;
+	game->win ? SDL_DestroyWindow(game->win) : 0;
 	if ((error = SDL_GetError()) && *error)
 		ft_fprintf(g_stderr, "wolf3d: %s\n", error);
 	SDL_Quit();
-	return error && *error ? 1 : 0;
+	exit(error && *error ? 1 : 0);
 }
 
-void *calculator(t_game *game)
+int calculator(t_game *game)
 {
-	while (game->running) {
-		sem_wait(&game->calculate);
-		puts("calculate !");
+	while (1)
+	{
+		SDL_SemWait(game->calculate);
+		if (!game->running)
+			break;
+		ft_printf("======> calculate !\n");
 	}
-	return NULL;
+	return 0;
 }
 
 
-void *renderer(t_game *game)
+int renderer(t_game *game)
 {
-	while (game->running) {
-		sem_wait(&game->render);
-		puts("render !");
+	while (1)
+	{
+		SDL_SemWait(game->render);
+		if (!game->running)
+			break;
+		ft_printf("======> render !\n");
 	}
-	return NULL;
+	return 0;
 }
 
-typedef void (t_keydown)(t_game *game, SDL_Event *event);
+typedef void (t_handler)(t_game *game, SDL_Event *event);
 
 void on_escape(t_game *game, SDL_Event *event)
 {
 	(void)game;
 	(void)event;
-	wl_quit();
+	wl_quit(game);
 }
 
 void on_r(t_game *game, SDL_Event *event)
 {
 	(void)event;
-	sem_post(&game->render);
+	SDL_SemPost(game->render);
 }
 
 void on_c(t_game *game, SDL_Event *event)
 {
 	(void)event;
-	sem_post(&game->calculate);
+	SDL_SemPost(game->calculate);
 }
 
-static t_keydown *g_keydown[UINT8_MAX] = {
+static t_handler *g_keydown[UINT8_MAX] = {
 	[SDLK_ESCAPE] = on_escape,
 	[SDLK_c] = on_c,
 	[SDLK_r] = on_r,
 };
 
+static t_handler *g_keyup[UINT8_MAX] = {
+};
+
 int game_loop(t_game *game)
 {
-	SDL_Event event;
+	SDL_Event ev;
 
-	while (game->running && SDL_WaitEvent(&event)) {
-		printf("Event: %d\n", event.key.keysym.sym);
-		switch (event.type) {
-			case SDL_QUIT:
-				return wl_quit();
-			case SDL_KEYDOWN:
-				if (event.key.keysym.sym < UINT8_MAX
-					&& g_keydown[event.key.keysym.sym])
-					g_keydown[event.key.keysym.sym](game, &event);
-				break;
-			default:
-				break;
-		}
-	}
-	return wl_quit();
+	while (game->running && SDL_WaitEvent(&ev))
+		if (ev.type == SDL_QUIT)
+			return wl_quit(game);
+		else if (ev.key.keysym.sym >= UINT8_MAX)
+			continue ;
+		else if (ev.type == SDL_KEYDOWN && g_keydown[ev.key.keysym.sym])
+			g_keydown[ev.key.keysym.sym](game, &ev);
+		else if (ev.type == SDL_KEYUP && g_keyup[ev.key.keysym.sym])
+			g_keyup[ev.key.keysym.sym](game, &ev);
+	return wl_quit(game);
 }
 
 int	main(int ac, char *av[])
@@ -108,19 +118,19 @@ int	main(int ac, char *av[])
 
 	(void)ac;
 	(void)av;
-	if (SDL_Init(SDL_INIT_VIDEO))
-		return wl_quit();
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO))
+		return wl_quit(&game);
 	game.running = TRUE;
-	if (sem_init(&game.calculate, 0, 0))
-		return wl_quit();
-	if (pthread_create(&game.calculator, NULL, (void *)calculator, &game))
-		return wl_quit();
-	if (sem_init(&game.render, 0, 0))
-		return wl_quit();
-	if (pthread_create(&game.renderer, NULL, (void *)renderer, &game))
-		return wl_quit();
+	if (!(game.calculate = SDL_CreateSemaphore(0)))
+		return wl_quit(&game);
+	if (!(game.renderer = SDL_CreateThread((void *)calculator, "01", &game)))
+		return wl_quit(&game);
+	if (!(game.render = SDL_CreateSemaphore(0)))
+		return wl_quit(&game);
+	if (!(game.renderer = SDL_CreateThread((void *)renderer, "02", &game)))
+		return wl_quit(&game);
 	if (!(game.win = SDL_CreateWindow("wolf3d", SDL_WINDOWPOS_UNDEFINED,
 		SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN)))
-		return wl_quit();
+		return wl_quit(&game);
 	return (game_loop(&game));
 }
